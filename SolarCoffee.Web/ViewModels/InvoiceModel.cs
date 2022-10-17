89369f9bdf493a402880ba97df68865dc8f25692
David Riggins <driggins@esri.com>
2022-10-17 11:12:44 -0700

    Lesson 12 End: Controllers, ViewModels, Mappers.

---
+++ SolarCoffee.Web/ViewModels/InvoiceModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SolarCoffee.Web.ViewModels
+{
+    public class InvoiceModel
+    {
+        /// <summary>
+        /// View model for open SalesOrders
+        /// </summary>
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public int CustomerId { get; set; }
+        public List<SalesOrderItemModel> LineItems { get; set; }
+    }
+
+
+    /// <summary>
+    /// View model for SalesOrderItems
+    /// </summary>
+    public class SalesOrderItemModel
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public ProductViewModel Product { get; set; }
+    }
+}
